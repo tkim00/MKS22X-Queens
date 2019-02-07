aae75a571d6929d41a5087a80a6bb2ec55fed7bc
Taejoon Kim <tkim00@stuy.edu>
2019-02-07 09:38:42 -0500

    constructor built

---
+++ QueenBoard.java
@@ -0,0 +1,11 @@
+public class QueenBoard {
+  private int[][]board;
+
+  public QueenBoard(int size) {
+    for (int x = 0; x < size; i++) {
+      for (int y = 0; y < size; i++) {
+        board[x][y] = 0;
+      }
+    }
+  }
+}
