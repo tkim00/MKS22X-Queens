dc68f600ee581d426bb29fb53c69c1e32bb0517a
Taejoon Kim <tkim00@stuy.edu>
2019-02-08 10:50:55 -0500

    toString made

--- QueenBoard.java
+++ QueenBoard.java
@@ -2,33 +2,40 @@ public class QueenBoard {
   private int[][]board;
 
   public QueenBoard(int size) {
-    for (int x = 0; x < size; i++) {
-      for (int y = 0; y < size; i++) {
-        board[x][y] = 0;
+    for (int r = 0; r < size; r++) {
+      for (int c = 0; c < size; c++) {
+        board[r][c] = 0;
       }
     }
   }
   
-  private boolean addQueen(int r, int c) {
+  public boolean addQueen(int r, int c) {
   	if (board[r][c]==0) {
   		board[r][c] = -1;
   		return true;			
   	}
   	return false;
   }
-  private boolean removeQueen(int r, int c) {
+  public boolean removeQueen(int r, int c) {
   	if (board[r][c]==-1) {
   		board[r][c] = 0;
   		return true;
   	}
   	return false;
   }
-  /*
+  
   public String toString() {
-  	for (int x = 0; x < board.length; x++) {
-  		for (int y = 0; y < board[x].length; y++) {
-  			
+  	String output = "";
+  	for (int r = 0; r < board.length; r++) {
+  		for (int c = 0; c < board[r].length; c++) {
+  			if (board[r][c] == -1) {
+  				output+="Q ";
+  			} else {
+  				output+="_ ";
+  			}
   		}
+  		output+="\n";
   	}
-  }*/
+  	return output;
+  }
 }
