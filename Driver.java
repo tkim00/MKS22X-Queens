ad66cec1918e19e62343435b339f649179d21ef8
Taejoon Kim <tkim00@stuy.edu>
2019-02-11 16:55:53 -0500

    testing solve() and working on it

--- Driver.java
+++ Driver.java
@@ -1,10 +1,18 @@
 public class Driver {
 	public static void main(String[] args) {
 		QueenBoard board = new QueenBoard(4);
+		/*
 		System.out.println(board.addQueen(0, 1));
 		System.out.println(board);
 		System.out.println(board.removeQueen(0, 0));
 		System.out.println(board.removeQueen(0, 1));
 		System.out.println(board);
+		*/
+		try {
+			System.out.println(board.solve());
+			System.out.println(board);
+		} catch(Exception IllegalStateException) {
+			System.out.println("Your board isn't refreshed yet!");
+		}
 	}
 }
--- QueenBoard.java
+++ QueenBoard.java
@@ -11,11 +11,11 @@ public class QueenBoard {
     }*/
     board = new int[size][size];
   }
-  
+
   private boolean addQueen(int r, int c) {
   	if (board[r][c]==0) {
   		board[r][c] = -1;
-  		return true;			
+  		return true;
   	}
   	return false;
   }
@@ -26,7 +26,7 @@ public class QueenBoard {
   	}
   	return false;
   }
-  
+
   public String toString() {
   	String output = "";
   	for (int r = 0; r < board.length; r++) {
@@ -34,11 +34,38 @@ public class QueenBoard {
   			if (board[r][c] == -1) {
   				output+="Q ";
   			} else {
-  				output+="_ ";
+  				output+="_ ";for (int x = 0; x < board.length; x++) {
+      solveR(x);
+    }
   			}
   		}
   		output+="\n";
   	}
   	return output;
+  }
+
+  public boolean solve() {
+    for (int r = 0; r < board.length; r++) {
+      for (int c = 0; c < board.length; c++) {
+        if (board[r][c]!=0) {
+          throw new IllegalStateException("Board has to be cleared!");
+        }
+      }
+    }
+    return solveR(0);
+  }
+  public boolean solveR(int col) {
+    if (col >= board.length) {
+      return true;
+    }
+    for (int r = 0; r < board.length; r++) {
+      if (addQueen(r, col)) {
+        if (solveR(col+1)) {
+          return true;
+        }
+        removeQueen(r, col);
+      }
+    }
+    return false;
   }
 }
